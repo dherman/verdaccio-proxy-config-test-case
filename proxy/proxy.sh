1c4cb1b04bd1a1b52d6bc5a3ba3deb5ea63649ee
David Herman <dherman@linkedin.com>
2023-10-23 09:23:44 -0700

    is the `PROXY` env variable clobbering something useful?

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PROXY=$1
+TEST_CASE_PROXY=$1
 
 # Boot the server in a background process.
-nohup npx verdaccio@6.0.0-6-next.76 --config ./config.yml --listen $PROXY &
+nohup npx verdaccio@6.0.0-6-next.76 --config ./config.yml --listen $TEST_CASE_PROXY &
 
 # Wait for the server to begin listening for connections
-( tail -F -n10 proxy.log & ) | fgrep -q $PROXY
+( tail -F -n10 proxy.log & ) | fgrep -q $TEST_CASE_PROXY
 
 cat proxy.log
